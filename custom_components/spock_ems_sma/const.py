93d45adc38a0d7c8e343f4bc037094cf43a49cd8
Enric <164487968+spocker-1@users.noreply.github.com>
2025-11-07 19:58:51 +0100

    Update const.py

--- custom_components/spock_ems_sma/const.py
+++ custom_components/spock_ems_sma/const.py
@@ -3,26 +3,21 @@ from datetime import timedelta
 DOMAIN = "spock_ems_sma"
 
 # --- Configuración de Polling (Telemetría SMA) ---
-# Intervalo fijo para leer datos del SMA Data Manager
 SCAN_INTERVAL_SMA = timedelta(seconds=10)
 
 # --- Configuración de la API de Spock (PUSH/PULL) ---
+# 1. Endpoint REMOTO (Outbound) - URL de tu API
+SPOCK_TELEMETRY_API_ENDPOINT = "https://ems-ha.spock.es/api/ems_marstek" 
 
-# 1. Endpoint REMOTO (Outbound)
-#    URL en la nube de Spock donde ENVIAMOS la telemetría
-SPOCK_TELEMETRY_API_ENDPOINT = "https://ems-ha.spock.es/api/ems_sma" 
-
-# 2. Path LOCAL (Inbound)
-#    Path local que registramos en HA para RECIBIR comandos
+# 2. Path LOCAL (Inbound) - Path para recibir comandos
 SPOCK_COMMAND_API_PATH = "/api/spock_ems_sma"
 
 # Claves para el config_flow y el data entry
 CONF_SPOCK_API_TOKEN = "spock_api_token"
 CONF_PLANT_ID = "plant_id"
+CONF_GROUP = "group"
+GROUPS = ["user", "installer"]
+DEFAULT_GROUP = "installer"
 
-# --- Constantes de SMA ---
 # Plataformas a cargar
 PLATFORMS = ["sensor"]
-
-# Grupo de usuario recomendado para SMA ennexOS
-SMA_DEFAULT_USERNAME = "installer"
