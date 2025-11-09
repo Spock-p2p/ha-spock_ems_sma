55eb8fa1601618c8468d91264b82050fe954e909
Enric <164487968+spocker-1@users.noreply.github.com>
2025-11-09 14:37:39 +0100

    Update const.py

--- custom_components/spock_ems_sma/const.py
+++ custom_components/spock_ems_sma/const.py
@@ -6,22 +6,19 @@ DOMAIN = "spock_ems_sma"
 SCAN_INTERVAL_SMA = timedelta(seconds=10)
 
 # --- Configuración de la API de Spock (PUSH/PULL) ---
-# 1. Endpoint REMOTO (Outbound) - URL de tu API
 SPOCK_TELEMETRY_API_ENDPOINT = "https://ems-ha.spock.es/api/ems_marstek" 
-
-# 2. Path LOCAL (Inbound) - Path para recibir comandos
 SPOCK_COMMAND_API_PATH = "/api/spock_ems_sma"
 
-# Claves para el config_flow y el data entry
+# Claves de configuración
 CONF_SPOCK_API_TOKEN = "spock_api_token"
 CONF_PLANT_ID = "plant_id"
 CONF_GROUP = "group"
 GROUPS = ["user", "installer"]
 DEFAULT_GROUP = "installer"
 
-# --- NUEVAS CONSTANTES DEL SWITCH ---
+# --- CONSTANTES DEL SWITCH MAESTRO ---
 MASTER_SWITCH_NAME = "Spock EMS SMA Control"
 MASTER_SWITCH_KEY = "master_control"
 
-# Plataformas a cargar (Añadido "switch")
+# Plataformas a cargar
 PLATFORMS = ["sensor", "switch"]
